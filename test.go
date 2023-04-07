31a9d98f9db549a65c5a4e1d7cc0ad866245fe54
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-07 15:57:46 -0700

    init

---
+++ test.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+	"bufio"
+	"os"
+	"log"
+)
+
+var pl = fmt.Println
+
+func main() {
+	pl("What is your name?")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+	if err == nil {
+		pl("Hello", name)
+	} else {
+		log.Fatal(err)
+	};
+}
+
