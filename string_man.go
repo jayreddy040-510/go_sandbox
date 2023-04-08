35a650757e87997ab3a3d95d4ad29e322ef4a3ab
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-08 16:35:29 -0700

    finishes up common string manipulation methods

--- string_man.go
+++ string_man.go
@@ -17,4 +17,8 @@ func main() {
 	fmt.Println("stringVar1 contains A:", strings.Contains(stringVar1, "A"))
 	fmt.Println("index of \"word\" in stringVar1:", strings.Index(stringVar1, "word"))
 	fmt.Println("replacing stringVar1 \"o\" with \"0\":", strings.Replace(stringVar1, "o", "0", -1))
+	untrimStr := "\nI\nAm\nNot\nTrimmed\n"
+	fmt.Println("Trim this str:", untrimStr, strings.TrimSpace(untrimStr))
+	fmt.Println("stringVar1 lowered:", strings.ToLower(stringVar1))
+	fmt.Println("stringVar1 uppered:", strings.ToUpper(stringVar1))
 }
