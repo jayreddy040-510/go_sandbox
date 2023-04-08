0dfe104b67f439020fad47bb5d20d00ac427d048
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-08 14:21:58 -0700

    adds Contains(), Index(), and replace() methods

--- string_man.go
+++ string_man.go
@@ -12,4 +12,9 @@ func main() {
 	replacer := strings.NewReplacer("word", "alligator")
 	stringVar1Replaced := replacer.Replace(stringVar1)
 	pl(stringVar1Replaced)
+	msg := fmt.Sprintf("length of stringVar1Replaced: %d", len(stringVar1Replaced))
+	pl(msg)
+	fmt.Println("stringVar1 contains A:", strings.Contains(stringVar1, "A"))
+	fmt.Println("index of \"word\" in stringVar1:", strings.Index(stringVar1, "word"))
+	fmt.Println("replacing stringVar1 \"o\" with \"0\":", strings.Replace(stringVar1, "o", "0", -1))
 }
