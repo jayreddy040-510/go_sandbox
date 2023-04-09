5ca74d3c9fcf204d2827da1eb0a53ad72278ff86
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-08 22:42:58 -0700

    adds for loop and formatted string rune evaluation in rune_manipulation.go file

--- rune_manipulation.go
+++ rune_manipulation.go
@@ -9,4 +9,8 @@ func main() {
 	rStr := "abcdefg"
 	runeCount := utf8.RuneCountInString(rStr)
 	fmt.Println("rune count:", runeCount)
+
+	for i, runeVal := range rStr {
+		fmt.Printf("%d: %#U: %c\n", i, runeVal, runeVal)
+	}
 }
