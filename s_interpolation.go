77e510ce9f5c7fe33812adc18272a678aa966688
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-09 11:52:29 -0700

    formatted printing

---
+++ s_interpolation.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// %d : Integer
+	// %c : Character
+	// %f : Float
+	// %t : Boolean
+	// %s : String
+	// %o : Base 8
+	// %x : Base 16
+	// %v : Guesses based on data type
+	// %T : Type of supplied value
+}
--- time_manipulation.go
+++ time_manipulation.go
@@ -9,4 +9,5 @@ import (
 func main() {
 	now := time.Now()
 	fmt.Println(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+
 }
