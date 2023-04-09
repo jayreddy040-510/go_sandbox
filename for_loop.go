6bd82adfeb37183ec9735e9b1f4915e20e9fc930
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-09 13:28:56 -0700

    adds long form for loop

---
+++ for_loop.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    for x := 5; x >= 1; x-- {
+        fmt.Printf("%d\n", x)
+    }
+
+
+}
--- s_interpolation.go
+++ s_interpolation.go
@@ -12,4 +12,6 @@ func main() {
 	// %x : Base 16
 	// %v : Guesses based on data type
 	// %T : Type of supplied value
+	fmt.Printf("%s %d %c %f %t %o %x\n",
+    "string", 3, 'A', 3.14, true, 2, 3)
 }
