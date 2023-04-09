74a332eac1042502a64bf60e84dc4a09ae82e7c8
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-08 22:46:49 -0700

    datetime obj similar to python datetime obj

---
+++ time_manipulation.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// behaves similarly to datetime in python
+func main() {
+	now := time.Now()
+	fmt.Println(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+}
