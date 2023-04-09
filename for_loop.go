14728f3296fd5bce85f37d8cd2f41700db3fe687
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-09 15:17:50 -0700

    adds fo rin syntax using range, adds to basic array structure

--- for_loop.go
+++ for_loop.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
+
+    // variable in for loop is block scoped
     for x := 5; x >= 1; x-- {
         fmt.Printf("%d\n", x)
     }
 
+    // can also use for keyword for while loops
+    fX := 0
+    for fX < 5 {
+        fmt.Println(fX)
+        fX++
+    }
 
+    arr := []string{"one", "two", "three"}
+
+    for i, str := range arr {
+     fmt.Println(i, str)
+    }
 }
