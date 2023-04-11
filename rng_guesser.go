57cc4a381a19d865264545afacd5f7f4b1563523
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-10 23:02:09 -0700

    finshes rng guesser, remember when using bufio.Reader().ReadString('\n') that it still captures the new line char.

--- rng_guesser.go
+++ rng_guesser.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "bufio"
-    "time"
-    "os"
-    "strconv"
-    "log"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 
 func main() {
     rand.Seed(time.Now().Unix())
     randNum := rand.Intn(10) + 1
+    fmt.Println(randNum)
+    userInputAsInt := takeUserInputReturnInt()
+    for userInputAsInt != randNum {
+        if userInputAsInt > randNum {
+            fmt.Println("Your guess is greater than the number!")
+        } else if userInputAsInt < randNum {
+            fmt.Println("Your guess is less than the number!")
+        }
+        userInputAsInt = takeUserInputReturnInt()
+    }
+    fmt.Println("Your guess is correct!")
+    return
+}
+
+func takeUserInputReturnInt() int {
     newReader := bufio.NewReader(os.Stdin) // NewReader() def arg is os.Stdin
-    userInput, err := newReader.ReadString() // ReadString() def arg is '\n'
+    userInput, err := newReader.ReadString('\n') // ReadString() def arg is '\n'
     if err == nil {
+        userInput = strings.TrimSpace(userInput)
         userInputAsInt, convErr := strconv.Atoi(userInput)
         if convErr != nil {
-            log.Fatal(convErr)   
+            return -1
         }
+        return userInputAsInt
+    } else {
+        return -1
     }
 }
