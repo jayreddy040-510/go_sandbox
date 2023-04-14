e16f9d45253460dda0fbd1e5b6ac4289bd305d86
Jay Reddy <jayreddy@Jays-Mac-mini.local>
2023-04-14 01:31:55 -0700

    trying out gofmt

--- leetcode/13_roman_to_integer.go
+++ leetcode/13_roman_to_integer.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-    fmt.Println(romanToInt("XVIIIXLCM"))
+	fmt.Println(romanToInt("XVIIIXLCM"))
 }
 
 func romanToInt(s string) int {
-    ret := 0
-    for idx, char := range(s) {
-        if char == 'I'{
-            if idx < len(s) - 1 {
-                x := s[idx + 1]
-                if x == 'V' || x == 'X' {
-                    ret--
-                } else {
-                    ret += 1
-                }
-            } else {
-                ret++
-            }
-        } else if char == 'V' {
-            ret += 5
-        } else if char == 'X' {
-            if idx < len(s) - 1 {
-                x := s[idx + 1]
-                if x == 'L' || x == 'C' {
-                    ret -= 10
-                } else {
-                    ret += 10
-                }
-            } else {
-                ret += 10
-            }
-        } else if char == 'L' {
-            ret += 50
-        } else if char == 'C' {
-            if idx < len(s) - 1 {
-                x := s[idx + 1]
-                if x == 'D' || x == 'M' {
-                    ret -= 100
-                } else {
-                    ret += 100
-                }
-            } else {
-                ret += 100
-            }
-        } else if char == 'D' {
-            ret += 500
-        } else if char == 'M' {
-            ret += 1000
-        }
-    }
-    return ret
+	ret := 0
+	for idx, char := range s {
+		if char == 'I' {
+			if idx < len(s)-1 {
+				x := s[idx+1]
+				if x == 'V' || x == 'X' {
+					ret--
+				} else {
+					ret += 1
+				}
+			} else {
+				ret++
+			}
+		} else if char == 'V' {
+			ret += 5
+		} else if char == 'X' {
+			if idx < len(s)-1 {
+				x := s[idx+1]
+				if x == 'L' || x == 'C' {
+					ret -= 10
+				} else {
+					ret += 10
+				}
+			} else {
+				ret += 10
+			}
+		} else if char == 'L' {
+			ret += 50
+		} else if char == 'C' {
+			if idx < len(s)-1 {
+				x := s[idx+1]
+				if x == 'D' || x == 'M' {
+					ret -= 100
+				} else {
+					ret += 100
+				}
+			} else {
+				ret += 100
+			}
+		} else if char == 'D' {
+			ret += 500
+		} else if char == 'M' {
+			ret += 1000
+		}
+	}
+	return ret
 }
